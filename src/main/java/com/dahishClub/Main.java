5356086322761482de9b72c79c2f3a5f5bb9d13a
JalaluddinKhafi <jalaluddin.khafi@gmail.com>
2025-12-19 23:37:16 +0430

    person class created

--- src/main/java/com/dahishClub/Main.java
+++ src/main/java/com/dahishClub/Main.java
@@ -1,10 +1,17 @@
 package com.dahishClub;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        List<String> names = List.of("Ali", "Mohammad", "Naser");
+        List<Integer> ages = List.of(20, 21, 22);
 
-
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i) + " " + ages.get(i));
+        }
     }
 }
