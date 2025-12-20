4c1d0d584bd3ba8ddc1e49cfb8569a60063d7a14
JalaluddinKhafi <jalaluddin.khafi@gmail.com>
2025-12-20 22:29:15 +0430

    new user added

--- src/main/java/com/dahishClub/Main.java
+++ src/main/java/com/dahishClub/Main.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        List<String> names = List.of("Ali", "Mohammad", "Naser");
-        List<Integer> ages = List.of(20, 21, 22);
+        List<String> names = List.of("Ali", "Mohammad", "Naser","Khafi");
+        List<Integer> ages = List.of(20, 21, 22, 25);
 
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i) + " " + ages.get(i));
